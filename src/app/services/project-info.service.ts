6f9f3d0ee80c55998339180ffcb64c35c39134d8
jromo1506 <jromo1506mx@gmail.com>
2025-03-08 21:50:07 -0600

    Commit

--- src/app/services/project-info.service.ts
+++ src/app/services/project-info.service.ts
@@ -17,6 +17,9 @@ export class ProjectInfoService {
           "../../assets/projects/dashaus/das3",
 
         ],
+        description:[
+          "Das Haus CRM is a real estate development management system built with the MEAN stack (MongoDB, Express.js, Angular, and Node.js). It is designed to optimize the administration of lots for construction companies, providing a robust, scalable, and modular solution for managing real estate development projects. The backend, developed with Node.js and Express.js, leverages RESTful APIs to ensure efficient communication between the frontend and database, implementing authentication, authorization, and data validation mechanisms. The system uses MongoDB as a NoSQL database, allowing flexible data structuring for property listings, sales transactions, customer records, and financial information. On the frontend, Angular is used to build a dynamic and responsive single-page application (SPA) with a component-based architecture that ensures maintainability and scalability. The platform supports cloud-based document storage for managing contracts, blueprints, and property-related files, while also integrating with third-party APIs such as payment gateways and email services for automated notifications. Additionally, it provides interactive dashboards with aggregated insights, sales trends, and financial reports to support data-driven decision-making. The application is hosted on AWS, ensuring high availability, performance, and secure cloud-based deployment. Das Haus CRM enhances operational efficiency in real estate development by automating workflows, improving collaboration among teams, and ensuring seamless management of properties, clients, and transactions."
+        ]
       }
 
     ],
